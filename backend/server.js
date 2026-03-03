6bc400e79c8cfdcb612e37c25f6827e18a774a9f
Shreepal Singh <rajpurohitshripal89@gmail.com>
2026-03-04 04:46:06 +0530

    fix: cors and api url

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:8081" 
-}));
-
+app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());
